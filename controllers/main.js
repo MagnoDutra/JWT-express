ffa776d51e24d9bdae0fc19f5367c3664c2a38e0
Magno <jmagno.dutra@hotmail.com>
2025-02-03 09:57:33 -0300

    Sign JWT

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,20 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 async function login(req, res) {
   const { username, password } = req.body;
 
   if (!username || !password)
     throw new CustomAPIError("Please provide email and password", 400);
-  res.send("Fake Login/Register/Signup");
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 }
 
 async function dashboard(req, res) {
